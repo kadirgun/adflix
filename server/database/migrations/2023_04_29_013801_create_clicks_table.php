dc5671e4870d01217832b99d0e36e7d782c57171
Kadir Gün <kadir@kadirgun.com>
2023-04-29 04:45:10 +0300

    modified 2023_04_29_013801_create_clicks_table.php in migrations

--- server/database/migrations/2023_04_29_013801_create_clicks_table.php
+++ server/database/migrations/2023_04_29_013801_create_clicks_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('clicks', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('link_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
