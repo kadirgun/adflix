a4a48462c674c612f8306c55003d1773d262b28c
Kadir Gün <kadir@kadirgun.com>
2023-04-30 10:07:14 +0300

    changed 4 files

--- server/database/migrations/2023_04_30_070523_create_click_requests_table.php
+++ server/database/migrations/2023_04_30_070523_create_click_requests_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('click_requests', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('avatar')->nullable();
-            $table->rememberToken();
+            $table->foreignId('click_id')->constrained()->cascadeOnDelete();
+            $table->json('headers');
+            $table->json('query');
+            $table->json('body');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('click_requests');
     }
 };
