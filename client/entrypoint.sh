d2626325038e73f13222caf1902d4c7c2ad91d16
Kadir Gün <kadir@kadirgun.com>
2023-05-02 21:56:45 +0300

    changed 8 files

--- client/entrypoint.sh
+++ client/entrypoint.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
 yarn install
-yarn dev
+
+if [ $APP_ENV = "local" ]
+then
+  echo "\033[33mRunning in local mode\033[0m"
+  yarn dev
+else
+  echo "\033[33mRunning in production/stagging mode\033[0m"
+  yarn build
+  yarn start
+fi
--- config/setup.sh
+++ config/setup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 docker compose run --rm -v $PWD/config:/config -w /config python j2 nginx.j2 > ./config/nginx.conf
-docker compose run --rm -v $PWD/config:/config -w /config python j2 nginx.dev.j2 > ./config/nginx.dev.conf
