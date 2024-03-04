7aa9234231ee8fa857c0050de211baba47622c8b
Kadir Gün <kadir@kadirgun.com>
2024-03-04 19:15:30 +0300

    Change advert type from Direct to Banner

--- server/app/Http/Controllers/AdvertController.php
+++ server/app/Http/Controllers/AdvertController.php
@@ -28,7 +28,7 @@ class AdvertController extends Controller {
             ], 404);
         }
 
-        $advert = Advert::query()->whereNotIn('category', $click->link->excluded_categories)->inRandomOrder()->where('type', AdvertType::Direct)->first();
+        $advert = Advert::query()->whereNotIn('category', $click->link->excluded_categories)->inRandomOrder()->where('type', AdvertType::Banner)->first();
 
         if(!$advert) {
             return response()->json([
